0204229208b15049171aa92690caabf906f3b4b1
Abhay-Hasrani <abhayhasrani@gmail.com>
2023-06-18 18:58:16 +0530

    added task 5

--- domManipulation_3/index.js
+++ domManipulation_3/index.js
@@ -9,3 +9,8 @@ items[2].style.backgroundColor = "green";
 for( let i of items){
     i.style.fontWeight = "bold";
 }
+
+//task 5
+var itemsWithTag = document.getElementsByTagName("li");
+// items[4].style.backgroundColor = "green"; //not working
+itemsWithTag[4].style.backgroundColor = "green"; //working
