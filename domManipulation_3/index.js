6a4f0e43394ac4323b150b63efe8afb9d1680db5
Abhay-Hasrani <abhayhasrani@gmail.com>
2023-06-18 19:14:16 +0530

    added task 6

--- domManipulation_3/index.js
+++ domManipulation_3/index.js
@@ -14,3 +14,16 @@ for( let i of items){
 var itemsWithTag = document.getElementsByTagName("li");
 // items[4].style.backgroundColor = "green"; //not working
 itemsWithTag[4].style.backgroundColor = "green"; //working
+
+//task 6
+// var seconditem = document.querySelector(".list-group-item:nth-child(2)");
+// seconditem.style.backgroundColor = "green";
+// var thirditem = document.querySelector(".list-group-item:nth-child(3)");
+// thirditem.style.visibility = "hidden";
+
+var itemsWithQSA = document.querySelectorAll(".list-group-item");
+itemsWithQSA[1].style.color = "green"
+var itemsWithQSAOdd = document.querySelectorAll(".list-group-item:nth-child(odd)");
+for(let i=0;i<itemsWithQSAOdd.length;i++){
+    itemsWithQSAOdd[i].style.backgroundColor = "green";
+}
