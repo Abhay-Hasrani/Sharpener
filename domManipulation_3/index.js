4c845d9ed5e95c4725d4e924c17bbc058637c81d
Abhay-Hasrani <abhayhasrani@gmail.com>
2023-06-18 18:19:18 +0530

    task 4 added

--- domManipulation_3/index.js
+++ domManipulation_3/index.js
@@ -1,4 +1,11 @@
 var title = document.getElementById("header-title");
 title.style.borderBottom  = "solid 3px black"
+
 var headingAddItems = document.getElementsByClassName("title");
 headingAddItems[0].style = "background-color:green;font-weight:bold;"
+
+var items = document.getElementsByClassName("list-group-item");
+items[2].style.backgroundColor = "green";
+for( let i of items){
+    i.style.fontWeight = "bold";
+}
