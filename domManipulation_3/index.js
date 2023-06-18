2b0252444b21ebc5eec96a8d76c1d517b8d37dc8
Abhay-Hasrani <abhayhasrani@gmail.com>
2023-06-18 18:03:54 +0530

    domManipulation_3 added

---
+++ domManipulation_3/index.js
@@ -0,0 +1,4 @@
+var title = document.getElementById("header-title");
+title.style.borderBottom  = "solid 3px black"
+var headingAddItems = document.getElementsByClassName("title");
+headingAddItems[0].style = "background-color:green;font-weight:bold;"
