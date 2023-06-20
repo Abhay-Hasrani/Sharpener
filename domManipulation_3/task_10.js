3f2df0f7fabcae0c0aaf4143818204e21467d4b5
Abhay-Hasrani <abhayhasrani@gmail.com>
2023-06-20 12:03:36 +0530

    task 10 added

--- domManipulation_3/task_10.js
+++ domManipulation_3/task_10.js
@@ -4,7 +4,14 @@ var emailText = document.getElementById("email");
 var form = document.getElementById("my-form");
 form.addEventListener("submit",(e)=>{
     e.preventDefault();
-    localStorage.setItem("name",nameText.value);
-    localStorage.setItem("email",emailText.value);
-    console.log("added ")
+    // localStorage.setItem("name",nameText.value);
+    // localStorage.setItem("email",emailText.value);
+
+    //task 11 storing as an object
+    let myobj = {
+        name : nameText.value,
+        email : emailText.value
+    };
+    localStorage.setItem("myobj",JSON.stringify(myobj));
+    console.log("added")
 });
