d779a0d1559294c2bec5231713149057b7a66bd0
Abhay-Hasrani <abhayhasrani@gmail.com>
2023-07-11 22:13:08 +0530

    updated list items

---
+++ First_React_App/01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,9 @@
+function ExpenseItem(){
+    return (
+    <div>
+    <h1>Expense List:</h1>
+    <p>Food Rs 10<br/>Petrol Rs 100<br/>Movies Rs 200</p>
+    </div>
+    );
+}
+export default ExpenseItem;
