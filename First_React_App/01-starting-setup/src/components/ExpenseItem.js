675907832ec012d0a2d9492f0313ac43f10a23e7
Abhay-Hasrani <abhayhasrani@gmail.com>
2023-07-12 18:35:39 +0530

    added expense list item css

--- First_React_App/01-starting-setup/src/components/ExpenseItem.js
+++ First_React_App/01-starting-setup/src/components/ExpenseItem.js
@@ -1,8 +1,16 @@
+import "./ExpenseItem.css"
 function ExpenseItem(){
     return (
-    <div>
-    <h1>Expense List:</h1>
-    <p>Food Rs 10<br/>Petrol Rs 100<br/>Movies Rs 200</p>
+    <div className="expense-item">
+    <div>July 12th 2023</div>
+    <div className="expense-item__description">
+        <h2>Food</h2>
+        <div className="expense-item__price">Rs 10</div>
+        <h2>Petrol</h2>
+        <div className="expense-item__price">Rs 100</div>
+        <h2>Movies</h2>
+        <div className="expense-item__price">Rs 200</div>
+    </div>
     </div>
     );
 }
