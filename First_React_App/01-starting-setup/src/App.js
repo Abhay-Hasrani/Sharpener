fd2e22b3a2cd52db57f1aba6a1cdb77a5984b320
Abhay-Hasrani <abhayhasrani@gmail.com>
2023-07-12 19:14:34 +0530

    communicationg between components {props}

--- First_React_App/01-starting-setup/src/App.js
+++ First_React_App/01-starting-setup/src/App.js
@@ -1,9 +1,40 @@
 import ExpenseItem from "./components/ExpenseItem";
 function App() {
+
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      location: "Bhandara"
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) ,location: "Warthi"},
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      location: "Mumbai"
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      location: "Pune"
+    },
+  ];
+  const expenseList = [];
+  for(let i=0;i<expenses.length;i++){
+    expenseList.push(
+    <ExpenseItem location ={expenses[i].location} title={expenses[i].title} date={expenses[i].date} amount={expenses[i].amount}></ExpenseItem>
+    );
+  }
   return (
     <div>
       <h2>Let's get started!!</h2>
-      <ExpenseItem></ExpenseItem>
+      {expenseList};
     </div>
   );
 }
--- First_React_App/01-starting-setup/src/components/ExpenseItem.js
+++ First_React_App/01-starting-setup/src/components/ExpenseItem.js
@@ -1,20 +1,12 @@
 import "./ExpenseItem.css"
-function ExpenseItem(){
-    const expenseDate = new Date();
-    const expenseDescription = ["Food","Petrol","Movies"];
-    const expensePrice = ["10","100","200"];
-    const expenseLocation = "Bhandara, Maharashtra"
+function ExpenseItem(props){
     return (
     <div className="expense-item">
-    <div>{expenseDate.toISOString()}</div>
-    <div>{expenseLocation}</div>
+    <div>{props.date.toUTCString()} </div>
+    <div> : {props.location}</div>
     <div className="expense-item__description">
-        <h2>{expenseDescription[0]}</h2>
-        <div className="expense-item__price">Rs {expensePrice[0]}</div>
-        <h2>{expenseDescription[1]}</h2>
-        <div className="expense-item__price">Rs {expensePrice[1]}</div>
-        <h2>{expenseDescription[2]}</h2>
-        <div className="expense-item__price">Rs {expensePrice[2]}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">Rs {props.amount}</div>
     </div>
     </div>
     );
