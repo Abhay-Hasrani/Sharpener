a4cfcde9469cf12f653d841387e9fd0e02af8ac9
Abhay-Hasrani <abhayhasrani@gmail.com>
2023-07-13 20:20:21 +0530

    saved state for each input

--- First_React_App/01-starting-setup/src/components/Expenses/ExpenseForm.js
+++ First_React_App/01-starting-setup/src/components/Expenses/ExpenseForm.js
@@ -1,17 +1,39 @@
-const ExpenseForm = () => {
-    function changeHandler(e){
-        console.log(e.target.value);
-    }
+import { useState } from "react";
 
-    return (
-        <form>
-            <label>Expense Description: </label>
-            <input onChange={changeHandler} type="text" id="title"/><br/>
-            <label>Expense Amount: </label>
-            <input onChange={changeHandler} type="number" id="amount"/><br/>
-            <label>Expense Date: </label>
-            <input onChange={changeHandler} type="date" id="date"/>
-        </form>
-    );
-}
+const ExpenseForm = () => {
+  const [enteredTitle, setTitle] = useState("");
+  const [enteredAmount, setAmount] = useState("");
+  const [enteredDate, setDate] = useState("");
+  function changeHandler(e) {
+    const id = e.target.id;
+    console.log(id);
+    switch (id) {
+      case "title":
+        setTitle(e.target.value);
+        console.log("title Updated");
+        break;
+      case "amount":
+        setAmount(e.target.value);
+        console.log("am0unt Updated");
+        break;
+      case "date":
+        setDate(e.target.value);
+        console.log("date Updated");
+        break;
+    }
+  }
+
+  return (
+    <form>
+      <label>Expense Description: </label>
+      <input onChange={changeHandler} type="text" id="title" />
+      <br />
+      <label>Expense Amount: </label>
+      <input onChange={changeHandler} type="number" id="amount" />
+      <br />
+      <label>Expense Date: </label>
+      <input onChange={changeHandler} type="date" id="date" />
+    </form>
+  );
+};
 export default ExpenseForm;
