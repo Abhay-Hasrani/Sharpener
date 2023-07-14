36bf74a6020fc5804e43dc69f093cf07143dc0ac
Abhay-Hasrani <abhayhasrani@gmail.com>
2023-07-14 21:04:47 +0530

    used conditional rendering to show message of no expenses are present

--- First_React_App/01-starting-setup/src/App.js
+++ First_React_App/01-starting-setup/src/App.js
@@ -32,45 +32,52 @@ const DUMMY_EXPENSES = [
     location: "Pune",
   },
 ];
+
 function App() {
   console.log("called");
-  
-  const [expenseListState, setExpenseList] = useState(DUMMY_EXPENSES);
-  const [filteredYear, setFilterYear] = useState('2023');
 
-  function filteredChangehandler(selectedYear){
+  const [expenseListState, setExpenseList] = useState(DUMMY_EXPENSES);
+  const [filteredYear, setFilterYear] = useState("2023");
+
+  function filteredChangehandler(selectedYear) {
     setFilterYear(selectedYear);
   }
 
   function OnAddExpense(expenseObj) {
     console.log(expenseObj);
-    //note always use new object dont pass old state object and 
+    //note always use new object dont pass old state object and
     // if state is dependent on previous stae use below syntax
-    setExpenseList(previousExpenseList =>{
-      return [expenseObj,...previousExpenseList];
-    }); 
+    setExpenseList((previousExpenseList) => {
+      return [expenseObj, ...previousExpenseList];
+    });
+  }
+  let filteredExpenses = expenseListState.filter((expense) => {
+    return expense.date.getFullYear() == filteredYear;
+  });
+  let expenseListContent = <p style={{ color: "white" }}>No Expenses Found</p>;
+
+  if (filteredExpenses.length > 0) {
+    expenseListContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        id={expense.id}
+        location={expense.location}
+        title={expense.title}
+        date={expense.date}
+        amount={expense.amount}
+      ></ExpenseItem>
+    ));
   }
 
   return (
     <div>
       <ExpenseForm onAddExpenseHandler={OnAddExpense} />
-      <ExpenseFilter selected = {filteredYear} onChangeFilter={filteredChangehandler}></ExpenseFilter>
-      {
-       expenseListState
-       .filter(expense=>{
-        return expense.date.getFullYear()==filteredYear;
-       })
-       .map((expense)=>(
-        <ExpenseItem
-            key={expense.id}
-            id={expense.id}
-            location={expense.location}
-            title={expense.title}
-            date={expense.date}
-            amount={expense.amount}
-          ></ExpenseItem>
-       ))
-      }
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filteredChangehandler}
+      ></ExpenseFilter>
+      {expenseListContent}
+      {(filteredExpenses.length === 1)?(<p style={{color : 'white',margin :"1rem"}}>Only single Expense here. Please add more...</p>):""}
     </div>
   );
 }
