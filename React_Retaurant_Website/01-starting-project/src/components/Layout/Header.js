a316c5306c78d40c33c5880910f2071117cdaa53
Abhay-Hasrani <abhayhasrani@gmail.com>
2023-07-26 21:36:11 +0530

    added meals item ad list with style

--- React_Retaurant_Website/01-starting-project/src/components/Layout/Header.js
+++ React_Retaurant_Website/01-starting-project/src/components/Layout/Header.js
@@ -2,7 +2,6 @@ import { Fragment } from "react";
 import clases from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpg";
 import HeaderCartButton from "./HeaderCartButton";
-import MealsSummary from "../Meal/MealsSummary";
 const Header = () => {
   return (
     <>
@@ -13,7 +12,6 @@ const Header = () => {
       <div className={clases["main-image"]}>
         <img src={mealsImage} alt="Table filled with food" />
       </div>
-      <MealsSummary/>
     </>
   );
 };
