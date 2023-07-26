b6ba21e7ee430ed7afd4daea185417792aee3804
Abhay-Hasrani <abhayhasrani@gmail.com>
2023-07-26 22:11:14 +0530

    added form inside meal item

--- React_Retaurant_Website/01-starting-project/src/components/Meal/MealItem/MealItem.js
+++ React_Retaurant_Website/01-starting-project/src/components/Meal/MealItem/MealItem.js
@@ -1,14 +1,17 @@
 import classes from "./MealItem.module.css"
+import MealItemForm from "./MealItemForm";
 const MealItem = (props) => {
   const formattedPrice = `$${props.price.toFixed(2)}`;
   return (
     <li className={classes.meal}>
       <div>
+      <div>
         <h3>{props.name}</h3>
       </div>
       <div className={classes.description}>{props.description}</div>
       <div className={classes.price}>{formattedPrice}</div>
-      <div></div>
+      </div>
+      <div><MealItemForm/></div>
     </li>
   );
 };
