86c5e55c07d6c7594dbb07cc060228c25ee65f0d
Abhay-Hasrani <abhayhasrani@gmail.com>
2023-08-02 21:47:20 +0530

    added aloading indicator

--- react-api-calls/01-starting-project/src/App.js
+++ react-api-calls/01-starting-project/src/App.js
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
+import Loader from "./components/UI/Loader";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films/");
     const jsonResponse = await response.json();
-    
     const moviesList = await jsonResponse.results.map((item) => {
       return {
         title: item.title,
@@ -16,6 +18,7 @@ function App() {
         openingText: item.opening_crawl,
       };
     });
+    setIsLoading(false);
     setMovies(moviesList);
   }
   return (
@@ -24,6 +27,7 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
+        {isLoading && <Loader/>}
         <MoviesList movies={movies} />
       </section>
     </React.Fragment>
