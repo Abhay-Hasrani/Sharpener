363f55ecdafd62ba61bd921dda07afe0a411cbc6
Abhay-Hasrani <abhayhasrani@gmail.com>
2023-08-04 19:43:42 +0530

    added feature so that token expires after 5 minutes and refreshing logs out

--- react-authentication/Authentication-main/src/store/UserProvider.js
+++ react-authentication/Authentication-main/src/store/UserProvider.js
@@ -2,17 +2,33 @@ import { useState } from "react";
 import UserContext from "./user-context";
 
 const UserProvider = (props) => {
-
+  let token = null;
+  let logInTime = null;
+  let obj = localStorage.getItem("idToken");
+  if(obj!=null) {
+    obj=JSON.parse(obj);
+    token = obj.token;
+    logInTime = obj.logInTime;
+  }
+  if (logInTime != null && Date.now() - logInTime > 5 * 60 * 1000) {
+    localStorage.setItem("idToken", JSON.stringify({
+      token: null,
+      logInTime: null,
+    }));
+  }
   const [userContext, setUserContext] = useState({
     //initialize it with local storage so refreshing doesnt log us out
-    idToken: localStorage.getItem("idToken"),
+    idToken: token,
     setIdToken: setIdTokenHandler,
   });
   function setIdTokenHandler(idToken) {
-    setUserContext((prev)=>{
-      localStorage.setItem("idToken",idToken);
-      return {...prev,idToken:idToken};
-    })
+    setUserContext((prev) => {
+      localStorage.setItem("idToken", JSON.stringify({
+        token: idToken,
+        logInTime: Date.now(),
+      }));
+      return { ...prev, idToken: idToken };
+    });
   }
   return (
     <UserContext.Provider value={userContext}>
